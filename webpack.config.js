2f50e2deff4386d86187f09dac7891c327381ead
Keegan Street <kestreet@deloitte.com.au>
2018-03-19 19:50:18 +1100

    Added plugin to extract CSS into separate file.

--- webpack.config.js
+++ webpack.config.js
@@ -1,22 +1,44 @@
+const ExtractTextPlugin = require('extract-text-webpack-plugin');
+const extractCSS = new ExtractTextPlugin('[name].css');
+
 module.exports = {
 	mode: process.env.WEBPACK_SERVE ? 'development' : 'production',
 	devtool: '#source-map',
 	module: {
 		rules: [{
 			test: /\.scss$/,
-			use: [{
-				loader: 'style-loader'
-			}, {
-				loader: 'css-loader',
-				options: {
-					sourceMap: true
+			oneOf: [
+				{
+					// If the extension is .js.scss, leave the CSS embedded in the JS file
+					test: /\.js\.scss$/,
+					use: [
+						'style-loader',
+						'css-loader',
+						'sass-loader'
+					]
+				}, {
+					// Otherwise, extract the CSS into its own file
+					use: extractCSS.extract({
+						use: [
+							{
+								loader: 'css-loader',
+								options: {
+									sourceMap: true
+								}
+							}, {
+								loader: 'sass-loader',
+								options: {
+									sourceMap: true
+								}
+							}
+						],
+						fallback: 'style-loader'
+					})
 				}
-			}, {
-				loader: 'sass-loader',
-				options: {
-					sourceMap: true
-				}
-			}]
+			]
 		}]
-	}
+	},
+	plugins: [
+		extractCSS
+	]
 };
